eb7389e7a0790d9355aeade76671103c84265bcc
Marcos Maia <maia.marcos@gmail.com>
2020-09-03 17:43:55 +0200

    enable Tomcat JMX for brand-service

---
+++ brand-service/src/main/java/io/maia/reactorplayground/brandservice/config/TomcatConfig.java
@@ -0,0 +1,17 @@
+package io.maia.reactorplayground.brandservice.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+  // enable tomcat JMX in sb 2.2
+  @Bean
+  public WebServerFactoryCustomizer<TomcatServletWebServerFactory> activateTomcatMBeanServer() {
+    return (factory) -> factory.setDisableMBeanRegistry(false);
+  }
+
+}
